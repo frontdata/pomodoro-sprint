a92d7f515545700c9fb5144494b89f0f1f5c526d
frontdata <61755611+frontdata@users.noreply.github.com>
2020-12-08 17:01:38 -0300

    Deploy inicial

---
+++ precache-manifest.00a639de86231e907d4551bdafcee4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ca9a25ddeff7adbc82123021318563",
+    "url": "/pomodoro-sprint/index.html"
+  },
+  {
+    "revision": "c35d77ad8569866a205f",
+    "url": "/pomodoro-sprint/static/css/main.73b6cb92.chunk.css"
+  },
+  {
+    "revision": "b27498396b6d9937e869",
+    "url": "/pomodoro-sprint/static/js/2.9cd6da6d.chunk.js"
+  },
+  {
+    "revision": "c35d77ad8569866a205f",
+    "url": "/pomodoro-sprint/static/js/main.d705a1db.chunk.js"
+  },
+  {
+    "revision": "2a59370e5cd944c2762c",
+    "url": "/pomodoro-sprint/static/js/runtime~main.d16ee262.js"
+  }
+]);
